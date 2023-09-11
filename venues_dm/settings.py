1970400c503a4fafc7938a1f3d40c8cd9837d118
Mustkeem Husain Sheikh <90866734+Mustkeem0910@users.noreply.github.com>
2023-09-12 00:02:05 +0530

    Update settings.py

--- venues_dm/settings.py
+++ venues_dm/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apis',
+    'venues_dm',
     'rest_framework',
     'corsheaders',
 ]
